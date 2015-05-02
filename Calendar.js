d82328d110b21fa778ddeeed58147e9783e943da
Mark <mark@remarkablemark.org>
2015-05-02 15:49:42 -0400

    Augmented HTMLElement with additional methods and fixed bug where last week of the month would not be rendered properly

--- Calendar.js
+++ Calendar.js
@@ -3,10 +3,44 @@
 "use strict";
 
 /**
+ * HTMLElement Methods
+ */
+// addClass
+HTMLElement.prototype.addClass = function(className) {
+    var elemClassString = this.className.trim(),
+        classes = className.trim().split(" "),
+        i;
+    
+    for (i = 0; i < classes.length; i++) {
+        // add class if no match is found
+        if (!elemClassString.match(new RegExp(classes[i]))) {
+            elemClassString += " " + classes[i].trim();
+        }
+    }
+    this.className = elemClassString;
+    return this;
+};
+
+// removeClass
+HTMLElement.prototype.removeClass = function(className) {
+    var elemClassString = this.className.trim(),
+        classes = className.trim().split(" "),
+        i;
+    
+    for (i = 0; i < classes.length; i++) {
+        // remove class by replacing it with an empty string if found
+        elemClassString = elemClassString.replace(classes[i].trim(), "");
+    }
+    this.className = elemClassString;
+    return this;
+};
+
+
+/**
  * Calendar Constructor
  */
 function Calendar() {
-    
+
     /**
      * init
      */
@@ -17,8 +51,10 @@ function Calendar() {
         day = today.getDay(),
         lastDateOfMonth = new Date(year, month + 1, 0).getDate(),
         firstDayOfMonth = new Date(year, month, 1).getDay(),
-        element = document.getElementById("calendar");
-    
+        element = document.getElementById("calendar"),
+        dayNames = ["Sunday", "Monday", "Tuesday", "Wednesday",
+                    "Thursday", "Friday", "Saturday"];
+
     /**
      * create method
      */
@@ -28,21 +64,13 @@ function Calendar() {
             tbody = createElem("tbody"),
             tr, th, td,
             dateNum,
-            months = ["January",
-                      "February",
-                      "March",
-                      "April",
-                      "May",
-                      "June",
-                      "July",
-                      "August",
-                      "September",
-                      "October",
-                      "November",
-                      "December"],
+            months = ["January", "February", "March", "April", "May",
+                      "June", "July", "August", "September",
+                      "October", "November", "December"],
             days = ["S", "M", "T", "W", "T", "F", "S"],
-            i, j;
-        
+            i, j, k,
+            dateElems, activeElem;
+
         // shorthand for document.createElement method
         function createElem(element, text) {
             var node,
@@ -69,10 +97,15 @@ function Calendar() {
             }
             return node;
         }
-        
+
         //--- create calendar header ---//
         // left arrow
-        tr = createElem("tr");
+        tr = createElem({
+            element: "tr",
+            attributes: {
+                class: "calendar-header"
+            }
+        });
         tr.appendChild(createElem({
             element: "th",
             text: "&lt;",
@@ -103,24 +136,17 @@ function Calendar() {
         for (i = 0; i < days.length; i++) {
             tr.appendChild(createElem("th", days[i]));
         }
-        tbody.appendChild(tr);
+        thead.appendChild(tr);
         tr = createElem("tr");
-        // append to table head
-        table.appendChild(thead);
         
         // iterate through the days of the month
         for (dateNum = 0; dateNum < lastDateOfMonth; dateNum++) {
-            //--- insert a new row at the end of every week --//
-            // last week of month
-            if (dateNum + 1 === lastDateOfMonth) {
-                tbody.appendChild(tr);
-            }
-            // every other week of month
-            else if (dateNum === 7 - firstDayOfMonth) {
+            // insert row at the end of the first week of the month
+            if (dateNum === 7 - firstDayOfMonth) {
                 tbody.appendChild(tr);
                 tr = createElem("tr");
             }
-            // first week of month
+            // insert row at the end of every other week of the month
             else if ((dateNum + firstDayOfMonth) % 7 === 0) {
                 tbody.appendChild(tr);
                 tr = createElem("tr");
@@ -134,20 +160,34 @@ function Calendar() {
             // insert date with dataset info
             td = createElem("td", dateNum + 1);
             td.dataset.date = year + "-" + ((month < 10) ? "0" + month : month) + "-" + (dateNum + 1);
-            // add class for easier querying
-            td.setAttribute("class", "js-calendar-date");
             // highlight current date
             if (dateNum + 1 === date) {
-                td.setAttribute("class", "active");
+                activeElem = td;
+                activeElem.addClass("active");
             }
             tr.appendChild(td);
+            // insert final row for the last week of month
+            if (dateNum + 1 === lastDateOfMonth) {
+                tbody.appendChild(tr);
+            }
         }
         // append to respective elements
+        table.appendChild(thead);
         table.appendChild(tbody);
         element.appendChild(table);
+        
+        dateElems = tbody.getElementsByTagName("td");
+        for (k = 0; k < dateElems.length; k++) {
+            dateElems[k].onclick = function() {
+                activeElem.removeClass("active");
+                activeElem = this;
+                activeElem.addClass("active");
+            };
+        }
     };
-    
+
 }
+
 
 // call
 var calendar = new Calendar();
